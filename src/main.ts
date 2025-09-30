9f8045750d2eb06dfeab111895d30bd3edfefd85
Uma Maheswara Rao <uvoleti00@mylangara.ca>
2025-09-30 16:16:22 -0700

    Cros error fix

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,10 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, { cors: true });
+  app.enableCors({
+    origin: 'https://ai-playground-ui-cyan.vercel.app',
+    methods: 'GET,POST',
+  });
   await app.listen(process.env.PORT ?? 3001);
 }
 bootstrap();
